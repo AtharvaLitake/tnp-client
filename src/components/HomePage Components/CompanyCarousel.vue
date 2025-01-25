13ebf213e14d1568fccb11de606359bf105919d4
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2025-01-25 11:07:19 +0530

    Company Logos Added

--- src/components/HomePage Components/CompanyCarousel.vue
+++ src/components/HomePage Components/CompanyCarousel.vue
@@ -7,7 +7,7 @@
                     <v-col v-for="company in companies" :key="company.id" cols="6" sm="4" lg="3"
                         class="d-flex justify-center">
                         <!-- Apply width and height explicitly -->
-                        <v-img :src="company.url"></v-img>
+                        <v-img :src="company.url" width="100"></v-img>
                     </v-col>
                 </v-row>
             </v-carousel-item>
@@ -28,22 +28,22 @@ export default {
     data() {
         return {
             companies: [{
-                id: 1, url: require('../../Images/Company Logos/PhonePe-Logo.png'), width: '200'
+                id: 1, url: require('../../Images/Company Logos/PhonePe-Logo.png')
             }, {
-                id: 2, url: 'https://www.energyexemplar.com/hs-fs/hubfs/Artwork/Energy%20Exemplar%20Logos/Energy%20Exemplar%20Logo%20Black%20(2)-1.png?width=854&height=504&name=Energy%20Exemplar%20Logo%20Black%20(2)-1.png', width: '100'
+                id: 2, url: require('../../Images/Company Logos/hsbc-logo.png'),
             }, {
-                id: 3, url: require('../../Images/Company Logos/PhonePe-Logo.png'), width: '200'
+                id: 3, url: require('../../Images/Company Logos/Siemens-Logo.png')
             }, {
-                id: 4, url: require('../../Images/Company Logos/PhonePe-Logo.png'), width: '200'
+                id: 4, url: require('../../Images/Company Logos/paloalto-logo.png')
             },
             {
-                id: 5, url: require('../../Images/Company Logos/PhonePe-Logo.png'), width: '200'
+                id: 5, url: require('../../Images/Company Logos/barclays-logo.png')
             },
             {
-                id: 6, url: require('../../Images/Company Logos/PhonePe-Logo.png'), width: '200'
+                id: 6, url: require('../../Images/Company Logos/eq-logo.png')
             },
             {
-                id: 7, url: require('../../Images/Company Logos/PhonePe-Logo.png'), width: '200'
+                id: 7, url: require('../../Images/Company Logos/aci-logo.jpg')
             },
             {
                 id: 8, url: 'https://www.freepnglogos.com/uploads/mastercard-png/mastercard-machine-learning-analytics-cloud-cloudera-6.png', width: '100'
