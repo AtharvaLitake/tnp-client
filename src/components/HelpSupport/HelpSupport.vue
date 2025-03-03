666f5a614ecc6f90528ae9c807c31f0a78e0ad47
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2025-03-03 09:32:36 +0530

    Help and SUpport Integrated

--- src/components/HelpSupport/HelpSupport.vue
+++ src/components/HelpSupport/HelpSupport.vue
@@ -7,32 +7,52 @@
       ensuring quick communication and efficient resolution of placement-related concerns.
     </p>
     <h1 class="text-h5 font-weight-bold text-primary my-5">Send Query</h1>
-    <v-form @submit.prevent>
+    <v-form @submit.prevent="submitquery">
       <v-textarea label="Enter your query" rows="6" v-model="query" flat counter="100"
         class="text-primary font-weight-medium" hint="Maximum 100 characters allowed" persistent-hint></v-textarea>
       <v-row class="d-flex justify-end">
         <v-col cols="4" class="d-flex justify-end">
-          <v-btn class="mt-4 bg-primary" size="x-large" type="submit">Send Query</v-btn>
+          <v-btn class="mt-4 bg-primary" size="x-large" type="submit" :loading="loading">Send Query</v-btn>
         </v-col>
       </v-row>
     </v-form>
-    
+
   </v-container>
   <app-footer></app-footer>
 </template>
 
 <script>
 import Nav from "@/components/BaseComponents/NavBar.vue";
+import axios from 'axios'
 export default {
   name: "HelpSupport",
-  components:{
-    'nav-bar':Nav
+  components: {
+    'nav-bar': Nav
   },
   data() {
     return {
-      query: '', // For binding the textarea input
+      loading:false,
+      query: '',
     };
   },
+  methods: {
+    submitquery() {
+      this.loading=true
+      const obj = {
+        description: this.query
+      }
+      axios.post('https://tnp-portal-backend-tpx5.onrender.com/api/v1/queries', obj)
+        .then(response => {
+          console.log('Query Sent Successfully', response.data);
+          this.query=''
+          this.loading=false
+        })
+        .catch(error => {
+          console.error(error);
+          this.loading=false
+        })
+    }
+  }
 };
 </script>
 
