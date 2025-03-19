32cbbea9b6b450cf20eebd87f2e1c3ea6877425f
ApurvaK <kulkarniapurva2003@gmail.com>
2025-03-19 18:17:18 +0530

    notices

--- src/components/GetNotifications.vue
+++ src/components/GetNotifications.vue
@@ -1,19 +1,14 @@
 <template>
     <nav-bar></nav-bar>
-    <v-container class="px-15">
-        <br>
-        <br>
-        <br>
-        <h1 class="text-h5 font-weight-bold text-primary mb-5">Notifications</h1>
-        <v-row no-gutters class="py-3 border-b-sm" v-for="notification in notifications" :key='notification.id'>
+    <v-container class="px-15 mt-15 ">
+        <h1 class="text-h5 font-weight-bold text-primary mb-4">Notices</h1>
+        <v-row no-gutters class="py-3 border-b-sm " v-for="notification in notifications" :key='notification.id'>
             <v-col cols="10" class="text-left">
                 <p class="text-h6 text-primary">{{ notification.title }}</p>
                 <p style="color:rgba(8, 30, 127, 0.6);">{{ notification.content }}</p>
             </v-col>
-            <v-col cols="2" class="text-right">
-                <v-icon class="text-primary h-100" size="large">mdi-delete-forever</v-icon>
-            </v-col>
         </v-row>
+        <br><br><br><br><br>
     </v-container>
     <app-footer></app-footer>
 </template>
@@ -34,6 +29,9 @@ export default {
                 },
                 {
                     id: 1, title: 'Barclays Regsitration Drive', content: 'Barclays is visiting campus on 24th September 2024 , register asap'
+                },
+                {
+                    id: 1, title: 'PhonePe Regsitration Drive', content: 'Submit your application before the deadline to avoid disqualification'
                 },
                 {
                     id: 1, title: 'PhonePe Regsitration Drive', content: 'Submit your application before the deadline to avoid disqualification'
